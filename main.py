8f7c4cb782d6330e9b14b51afb1cb67956ca64f0
EricMa <madihsiang@gmail.com>
2021-01-05 17:26:22 +0800

    flask login

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # flask
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import request, render_template, url_for, redirect, flash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
+
+
 
 # sqlite
 import sqlite3 as DB
 
 #定義資料庫位置
-sqlite_path = 'D:\ptt.db'
+sqlite_path = 'ptt.db'
 
 
 def queryData(sqlite_path,webName):
@@ -35,11 +38,63 @@ def get_DB(DB_path):
     curr = conndb.cursor()  
     return [conndb,curr]
 
+# 設定密碼
+import os
+secret_key = os.urandom(16).hex()
 
 app = Flask(__name__)
+app.secret_key = secret_key
+
+#初始化Flask-Login
+login_manager = LoginManager()
+
+#將flask與flask-login綁定
+login_manager.init_app(app)
+
+#預設是'basic'，這行可寫可不寫
+# login_manager.session_protection = "strong"
+
+#當未登入的使用者請求了一個需要權限的網頁時，就將他送到代表login()的位址去，login()是函式
+login_manager.login_view = 'login'
+
+#當未登入的使用者被送到login_view所指定的位址時，會一併跳出的訊息
+login_manager.login_message = '請輸入帳號密碼已登入'
+
+#繼承Flask-login裏頭的UserMixin
+class User(UserMixin):
+    pass
+
+#驗證使用者是否存在在合法清單內
+@login_manager.user_loader
+def user_loader(loginUser):
+    if loginUser not in users:
+        return
+
+    user = User()
+    user.id = loginUser
+    return user
+
+#驗證使用者的密碼是否正確
+@login_manager.request_loader
+def request_loader(request):
+    loginUser = request.form.get('user_id')
+    if loginUser not in users:
+        return
+
+    user = User()
+    user.id = loginUser
+
+    # user.is_authenticated = (request.form['password'] == users[loginUser]['password'])
+
+    return user
+
+#建立一個使用者清單
+users = {'Madi': {'password': '0914'}}
+
 
 # 一定要有GET
 @app.route('/',methods=['GET','POST'])
+@login_required #指定該頁面一定要登入才能查看
 def home():
     if request.method == 'GET':
         boardName = queryBoardName(sqlite_path)
@@ -55,6 +110,31 @@ def home():
         #     datas = []
         #     return render_template('view.html', datas=datas, webName='No Such Table')
         
+#登入
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    
+    loginUser = request.form['user_id']
+    if (loginUser in users) and (request.form['password'] == users[loginUser]['password']):
+        user = User()
+        user.id = loginUser
+        login_user(user)
+        flash(f'{loginUser}！歡迎登入！')
+        return redirect(url_for('home'))
+
+    flash('登入失敗了...')
+    return render_template('login.html')
+
+#登出
+@app.route('/logout')
+def logout():
+    loginUser = current_user.get_id()
+    logout_user()
+    flash(f'{loginUser}！歡迎下次再來！')
+    return render_template('login.html')
+
 
 # 一定要有GET
 # @app.route('/queryResults',methods=['GET','POST'])
